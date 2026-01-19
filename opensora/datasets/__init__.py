94e0bf0514f3ed3f93c5be32a1e7017333d4ee71
jzndd <1274669808@qq.com>
2026-01-19 07:11:55 +0000

    fix bugs

--- opensora/datasets/__init__.py
+++ opensora/datasets/__init__.py
@@ -1,2 +1,3 @@
-from .datasets import IMG_FPS, BatchFeatureDataset, VariableVideoTextDataset, VideoTextDataset
+# from .datasets import IMG_FPS, BatchFeatureDataset, VariableVideoTextDataset, VideoTextDataset
 from .utils import get_transforms_image, get_transforms_video, is_img, is_vid, save_sample
+from .datasets import IMG_FPS
